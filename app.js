e2c31341a294dd3e1015c72ff8eae047a0ea4379
Vaishnavi-Raykar <vaishnaviraykar990@gmail.com>
2024-02-09 21:43:54 +0530

    final commit

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const users = require('./routes/users');
 var app = express();
 
 // view engine setup
+console.log("hello")
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
